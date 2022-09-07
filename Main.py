2c32cf1499fda0ccd0bac66ee4d45d454ca5bdfb
urk21cs2041 <110519141+urk21cs2041@users.noreply.github.com>
2022-09-07 08:57:55 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 class Node:
-    def _init_(self, data=None):
+    def __init__(self, data=None):
         self.data = data
         self.previous = self
         self.next = self
 
 
 class DoublyCircularLinkedList:
-    def _init_(self):
+    def __init__(self):
         self.head = None
         self.count = 0
 
